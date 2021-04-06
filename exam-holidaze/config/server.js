d3c36db9a6a79b077c7ffea7465ccfbb54e5932f
Christian larsen <Cskilbredl.1993@gmail.com>
2021-04-06 11:43:21 +0200

    initial files

---
+++ exam-holidaze/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e330461410724c8e0e3dcb845c0614b9'),
+    },
+  },
+});
---
+++ exam-holidaze/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3ab4dc14-6548-4a08-923b-a613ef40cf9c'
+};
